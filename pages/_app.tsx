a96c5f9bb6f3191f9e388cd7237808e43b2a57bc
Ihor Berezhnyi <iberezhnyi@gmail.com>
2024-10-20 10:45:24 +0300

    feature(global styles, structure): add global styles, change structure

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+import { Poppins } from 'next/font/google'
+import Head from 'next/head'
+
+const poppins = Poppins({
+  weight: ['400', '500', '600'],
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Create Next App</title>
+      </Head>
+      <div className={poppins.className}>
+        <Component {...pageProps} />
+      </div>
+    </>
+  )
 }
