6a7ef39caac49673b9d4345925e1a28082ece4f3
Ihor Berezhnyi <iberezhnyi@gmail.com>
2024-10-21 18:12:54 +0300

    feature(header): add header markup

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,7 +1,62 @@
 import { FC } from 'react'
+import './header.css'
 
 const Header: FC = () => {
-  return <header>Header</header>
+  return (
+    <header className="header">
+      <nav className="nav container">
+        <a href="index.html" className="nav__logo">
+          Smith
+        </a>
+
+        <div className="nav__menu">
+          <ul className="nav__list grid">
+            <li className="nav__item">
+              <a href="#home" className="nav__link active-link">
+                <i className="uil uil-estate nav__icon"></i>Home
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#about" className="nav__link">
+                <i className="uil uil-user nav__icon"></i>About
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#skills" className="nav__link">
+                <i className="uil uil-file-alt nav__icon"></i>Skills
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#services" className="nav__link">
+                <i className="uil uil-briefcase-alt nav__icon"></i>Services
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#portfolio" className="nav__link">
+                <i className="uil uil-scenery nav__icon"></i>Portfolio
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#contacts" className="nav__link">
+                <i className="uil uil-message nav__icon"></i>Contacts
+              </a>
+            </li>
+          </ul>
+
+          <i className="uil uil-times nav_close"></i>
+        </div>
+
+        <div className="nav_toggle">
+          <i className="uil uil-apps"></i>
+        </div>
+      </nav>
+    </header>
+  )
 }
 
 export default Header
